b75ccaf49338b011be5e1bdde0334f2a6f414133
Rainer Sigwald <raines@microsoft.com>
2021-11-19 20:49:57 +0000

    if only

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ try {
     // const payload = JSON.stringify(github.context.payload, undefined, 2)
     // console.log(`The event payload: ${payload}`);
 
-    console.log(`base branch: ${base.context.payload.pull_request.ref}`);
+    console.log(`base branch: ${github.context.payload.pull_request.base.ref}`);
 } catch (error) {
     core.setFailed(error.message);
 }
